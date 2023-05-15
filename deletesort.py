6e21879daf08dad445512113369d09482cd3bbae
Victor Curalea <victor.curalea@gmail.com>
2023-05-15 23:46:53 +0200

    Update deletesort.py

--- deletesort.py
+++ deletesort.py
@@ -5,13 +5,17 @@ with open('file1.csv', 'r') as f:
     reader = csv.reader(f)
     file1_dict = {row[1]: row[0] for row in reader}
 
-# Open the second file and a new file for output
-with open('file2.csv', 'r') as f, open('output.csv', 'w', newline='') as out:
+# Read the second file and build a set of URLs
+with open('file2.csv', 'r') as f:
     reader = csv.reader(f)
+    file2_urls = set(row[1] for row in reader)
+
+# Open a new file for output
+with open('output.csv', 'w', newline='') as out:
     writer = csv.writer(out)
 
-    # For each line in the second file, write to the output file
-    # only if the URL exists in the dictionary
-    for row in reader:
-        if row[1] in file1_dict:
-            writer.writerow(row)
+    # For each URL in the dictionary, write to the output file
+    # only if the URL exists in the set
+    for url, id in file1_dict.items():
+        if url in file2_urls:
+            writer.writerow([id, url])
